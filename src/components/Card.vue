0fa34cb580b3ab19dedc3fc996ced856697a6d88
Pape Barka <papebarka@gmail.com>
2023-08-22 16:36:09 +0000

    Adds component

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue'
+
+    const emit = defineEmits(['complete', 'delete', 'edit'])
+</script>
+
+<template>
+    <div class="card">
+        <section>
+            <div>
+                <slot name="header" />
+            </div>
+            <div>
+                <button @click="click('edit')">Edit</button>
+                <button @click="click('delete')">Delete</button>
+                <button @click="click('complete')">Completed</button>
+            </div>
+        </section>
+        <section>
+            <slot name="content" />
+        </section>
+    </div>
+</template>
